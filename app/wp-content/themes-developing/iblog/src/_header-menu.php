2fa9b9890012eb0cb277188e2a0947e0b92a95b3
pepe <pepe@pprios.com>
2018-06-24 22:55:55 -0500

    [theme]:update menu mobile

--- app/wp-content/themes-developing/iblog/src/_header-menu.php
+++ app/wp-content/themes-developing/iblog/src/_header-menu.php
@@ -23,9 +23,8 @@
 				<?php
 					wp_nav_menu( array(
 						'theme_location' => 'primary',
-						'menu_class'     => 'primary-menu',
 						'container' => false,
-						'menu_class' => 'menu'
+						'menu_class' => 'menu clearfix'
 					 ) );
 				?>
 			</nav>
